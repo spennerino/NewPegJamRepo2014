09f1cd756a7eb329fe0cbb4acb571095ca655f02
Joshua Grand <jgrand@live.ca>
2014-07-25 20:45:52 -0500

    Added BearChase script.

---
+++ GrizzlyIntentions/Assets/Scripts/BearChase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearChase : MonoBehaviour
+{
+
+    public Transform victim;
+    private NavMeshAgent navComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        navComponent = this.transform.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (victim)
+        {
+            navComponent.SetDestination(victim.position);
+        }
+    }
+}
