efb448f875fa996ce07a20a9282ddc23af2dbda6
spennerino <slaughterism@hotmail.com>
2014-07-25 21:54:21 -0500

    Added Bear to scene

--- GrizzlyIntentions/Assets/Scripts/BearKill.cs
+++ GrizzlyIntentions/Assets/Scripts/BearKill.cs
@@ -13,6 +13,20 @@ public class BearKill : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+	}
 
+	void OnCollisionEnter(Collision collision)
+	{
+		// Debug-draw all contact points and normals
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			
+		}
+
+		// Play a sound if the colliding objects had a big impact.		
+		if (collision.relativeVelocity.magnitude > 1)
+		{
+
+		}	
 	}
 }
