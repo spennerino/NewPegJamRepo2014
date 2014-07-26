063da7ebbb2129a900fae00dce3b51205179dd07
Joshua Grand <jgrand@live.ca>
2014-07-25 23:36:12 -0500

    Added Punch script

---
+++ GrizzlyIntentions/Assets/Scripts/Punch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Punch : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray frontCheck = new Ray(this.transform.position, Vector3.forward);
+        Debug.DrawRay(this.transform.position, Vector3.forward * 10);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(frontCheck, out hit, 10))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    hit.rigidbody.AddForce(Vector3.forward * 10000);
+                }
+            }
+        }
+    }
+}
